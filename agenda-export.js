4fb6485d1404bd489fc21a3e75e11242fdb128c8
svanbergen99 <s.vanbergen99@gmail.com>
2026-09-02 15:15:53 +0200

    Load external website shortcuts

--- agenda-export.js
+++ agenda-export.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const VERSION = "20260902-35";
+  const VERSION = "20260902-36";
   const FILE_MONTHS = ["Januari","Februari","Maart","April","Mei","Juni","Juli","Augustus","September","Oktober","November","December"];
   const MONTH_FILE_RE = /^Roosterindex_(Januari|Februari|Maart|April|Mei|Juni|Juli|Augustus|September|Oktober|November|December)\.json$/i;
 
@@ -105,6 +105,7 @@
   loadStyle("break-calculator.css");
   loadStyle("personal-month.css");
   loadStyle("permission-auth.css");
+  loadStyle("external-sites.css");
 
   loadScript("roster-controller.js");
   loadScript("access-permissions.js");
@@ -123,4 +124,5 @@
   loadScript("break-calculator.js");
   loadScript("screenshot-theme.js");
   loadScript("personal-month.js");
+  loadScript("external-sites.js");
 })();
