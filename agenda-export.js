5814b6c1924128af3bd107746dc27c7801ed3dac
svanbergen99 <s.vanbergen99@gmail.com>
2026-09-02 12:51:40 +0200

    Load traffic dashboard shortcut

--- agenda-export.js
+++ agenda-export.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const VERSION = "20260902-28";
+  const VERSION = "20260902-29";
   const FILE_MONTHS = ["Januari","Februari","Maart","April","Mei","Juni","Juli","Augustus","September","Oktober","November","December"];
   const MONTH_FILE_RE = /^Roosterindex_(Januari|Februari|Maart|April|Mei|Juni|Juli|Augustus|September|Oktober|November|December)\.json$/i;
 
@@ -119,6 +119,7 @@
   loadScript("workers-view.js");
   loadScript("salary-payments.js");
   loadScript("break-calculator.js");
+  loadScript("traffic-link.js");
   loadScript("screenshot-theme.js");
   loadScript("personal-month.js");
 })();
