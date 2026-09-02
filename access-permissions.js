2a837afc67b831b97accbeb8a6cbc0ed61b3e8ba
svanbergen99 <s.vanbergen99@gmail.com>
2026-09-02 11:47:46 +0200

    Allow first-name permission logins

--- access-permissions.js
+++ access-permissions.js
@@ -2,7 +2,13 @@
   "use strict";
 
   window.RoosterAccessPermissions = Object.freeze([
-    "46e31a7960ad20f133d6ee0e9a08c00df06f75ad2b223be26befc453b7e93a1a",
-    "68795be40769c09a0485295fd16b2df51097a010136a787f7d04c4cfad31e122"
+    Object.freeze({
+      loginHash: "1409f10ff7af70c0c13e63393e634c6be016cfdb7a18dad262d854b2bb4b5e03",
+      rosterHash: "46e31a7960ad20f133d6ee0e9a08c00df06f75ad2b223be26befc453b7e93a1a"
+    }),
+    Object.freeze({
+      loginHash: "7b50943dab3f7f92f4120ca9a031d777b5a13bb71749e7e6a2cdd5f2e0ddaf5a",
+      rosterHash: "68795be40769c09a0485295fd16b2df51097a010136a787f7d04c4cfad31e122"
+    })
   ]);
 })();
