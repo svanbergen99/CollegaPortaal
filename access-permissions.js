19011f6b07ee9d3cf5e2643fdbaa4501c63eca6e
svanbergen99 <s.vanbergen99@gmail.com>
2026-09-02 11:36:16 +0200

    Add Sana roster permission

--- access-permissions.js
+++ access-permissions.js
@@ -2,6 +2,7 @@
   "use strict";
 
   window.RoosterAccessPermissions = Object.freeze([
-    "46e31a7960ad20f133d6ee0e9a08c00df06f75ad2b223be26befc453b7e93a1a"
+    "46e31a7960ad20f133d6ee0e9a08c00df06f75ad2b223be26befc453b7e93a1a",
+    "68795be40769c09a0485295fd16b2df51097a010136a787f7d04c4cfad31e122"
   ]);
 })();
