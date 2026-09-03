2c354eff4f2d6e4f124dbff2d8635c52ce5dec27
svanbergen99 <s.vanbergen99@gmail.com>
2026-09-03 11:38:28 +0200

    Add Compliance Check link to work websites

--- external-sites.js
+++ external-sites.js
@@ -13,6 +13,7 @@
         LEAVE_REQUEST,
         Object.freeze({ label: "1MDW", url: "https://azkrplbs001.az.unix.corp:44300/sap(bD1ubCZjPTEwMCZkPW1pbg==)/bc/bsp/sap/crm_ui_start/default.htm" }),
         Object.freeze({ label: "Brein", url: "https://brein-sio-particulier.custhelp.com/app/home/" }),
+        Object.freeze({ label: "Compliance Check", url: "https://svanbergen99.github.io/Checklist/" }),
         Object.freeze({ label: "Noodprocedure formulier", url: "https://achmea.sharepoint.com/sites/SP-15261/Noodprocedures/Noodprocedures.aspx", warning: "Alleen gebruiken als Traffic toestemming geeft" }),
         Object.freeze({ label: "Werkbriefjes / Loonstrook", url: "https://klantcontactdiensten.nocore.nl/" }),
         Object.freeze({ label: "Wallboard", url: "https://achmea-production-1-a3srealtime-eu-west-1-prod.kb.eu-west-1.aws.found.io/s/centraal-beheer/app/dashboards#/view/731a7b2c-c25f-4ff6-a032-5f62ef6d2272/7f508da0-6f02-4e64-8ec7-edf69a87aa56?_g=(filters:!())" }),
